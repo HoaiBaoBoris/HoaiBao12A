ce1864dba4d4c9c1c9591c7921c1ebb260ab7c38
HoaiBaoBoris <bao26062007@gmail.com>
2025-03-25 20:22:54 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>THANH THUÝ</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+  <body>
+    <div class =“header”> TRƯỜNG THPT NGÔ QUYỀN </div>
+<div class=“topnav”>
+<a href =“nv1.html” target="noidung"> Nhiệm vụ 1 </a>
+<a href =“tkb.html”> Thời khoá biểu </a>
+</div>
+<div class =“cot” > </div>
+<div class =“cot1.side” > </div>
+<div class =“cot2.middle” > </div>
+    <iframe name = "noidung"></iframe>
+<div class=“footer”>Thông tin</div>
+
+    <form>
+      <label> Tên đăng nhập: </label>
+      <input type="text" id="username" name="username">
+      <label> Mật khẩu: </label>
+      <input type="text" id="password" name="password">
+      <button type="submit"onclick=login()> Đăng nhập </button>
+    </form>
+    <script scr="script.js"> </script>
+      
+    </script>
+  </body>
+</html>
